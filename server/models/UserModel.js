dca5f81367a201c34a38df3dccaaaad9579b5d6e
Oguzhan <oguzhanturgut@outlook.com>
2020-03-27 17:33:23 +0100

    add password hashing

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const emailValidator = require('email-validator');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 12;
 
 const UserSchema = mongoose.Schema({
   username: {
@@ -36,5 +39,21 @@ const UserSchema = mongoose.Schema({
 }, {
   timestamps: true,
 });
+
+UserSchema.pre('save', async function preSave(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  try {
+    const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+    user.password = hash;
+    return next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+UserSchema.methods.comparePassword = async function comparePassword(pass) {
+  return bcrypt.compare(pass, this.password);
+};
 
 module.exports = mongoose.model('user', UserSchema);
